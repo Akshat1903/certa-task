38bd18d5d895391b1b6e8d0d7563de65c3a5123e
Akshat1903 <akshatgupta1903@gmail.com>
2022-06-22 04:00:39 +0530

    display messages modified

--- songspotter/main.py
+++ songspotter/main.py
@@ -6,8 +6,12 @@ input_artist = input("Enter artist name: ")
 
 artists = get_artists(input_artist)
 
+print('\n')
+print("These are the top 3 search results - ")
 for i, e in enumerate(artists):
     print(f"{i + 1} - {e['name']}")
+
+print('\n')
 
 while True:
     option = input("Choose artist index to see his/her top tracks - ")
@@ -20,6 +24,8 @@ while True:
     except:
         print("Please choose a valid index !!")
 
+print('\n')
+print(f"These are the top 3 tracks of {artists[option - 1]['name']} - ")
 artist_id = artists[option - 1]["uri"].split(":")[2]
 
 tracks = get_tracks(artist_id)
@@ -27,4 +33,5 @@ tracks = get_tracks(artist_id)
 for i, t in enumerate(tracks):
     print(f"{i + 1} - {t['name']}: {t['external_urls']['spotify']}")
 
+print('\n')
 print("***** Thank you for using the program! *****")
